58de4923373bbd175ae5b5bc02203469e90e91eb
kumar samyak <kumar.samyak018@gmail.com>
2024-04-02 16:37:52 +0530

    oibsip_Project2level2

---
+++ Oasis-Infobyte-Project2-Level2.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from tkinter import *
+import numpy as np
+def showQuality():
+    new = np.array([[float(e1.get()),float(e2.get()),float(e3.get()),float(e4.get()),float(e5.get()),float(e6.get()),float(e7.get()),float(e8.get()),float(e9.get()),float(e10.get()),float(e11.get())]])
+    Ans = RF_clf.predict(new)
+    fin=str(Ans)[1:-1]
+    quality.insert(0, fin)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+data = pd.read_csv(r"C:\Users\kumar\Downloads\Oasis infobyte internship\Winequalityprediction-Project2-Level2\WineQT.csv")
+data.head()
+data.describe()
+extra = data[data.duplicated()]
+extra.shape
+y = data.quality                  
+X = data.drop('quality', axis=1)  
+print(y.shape, X.shape)
+colormap = plt.cm.viridis
+plt.figure(figsize=(12,12))
+plt.title('Correlation of Features', y=1.05, size=15)
+sns.heatmap(data.astype(float).corr(),linewidths=0.1,vmax=1.0, square=True, 
+            linecolor='white', annot=True)
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.metrics import confusion_matrix
+seed = 8 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=seed)
+RF_clf = RandomForestClassifier(random_state=seed)
+cv_scores = cross_val_score(RF_clf,X_train, y_train, cv=10, scoring='accuracy')
+RF_clf.fit(X_train, y_train)
+pred_RF = RF_clf.predict(X_test)
+master = Tk()
+Label(master, text="Fixed Acidity", anchor="nw", width=15).grid(row=0)
+Label(master, text="Volatile Acidity", anchor="nw", width=15).grid(row=1)
+Label(master, text="Citric Acid", anchor="nw", width=15).grid(row=2)
+Label(master, text="Residual Sugar", anchor="nw", width=15).grid(row=3)
+Label(master, text="Chlorides", anchor="nw", width=15).grid(row=4)
+Label(master, text="free sulphur", anchor="nw", width=15).grid(row=5)
+Label(master, text="Total Sulfur", anchor="nw", width=15).grid(row=6)
+Label(master, text="Density", anchor="nw", width=15).grid(row=7)
+Label(master, text="pH", anchor="nw", width=15).grid(row=8)
+Label(master, text="Sulphates", anchor="nw", width=15).grid(row=9)
+Label(master, text="Alcohol", anchor="nw", width=15).grid(row=10)
+Label(master, text = "Quality", anchor="nw", width=15).grid(row=13)
+Label(master, text = "Id", anchor="nw", width=15).grid(row=13)
+e1 = Entry(master)
+e2 = Entry(master)
+e3 = Entry(master)
+e4 = Entry(master)
+e5 = Entry(master)
+e6 = Entry(master)
+e7 = Entry(master)
+e8 = Entry(master)
+e9 = Entry(master)
+e10 = Entry(master)
+e11 = Entry(master)
+quality = Entry(master)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+e3.grid(row=2, column=1)
+e4.grid(row=3, column=1)
+e5.grid(row=4, column=1)
+e6.grid(row=5, column=1)
+e7.grid(row=6, column=1)
+e8.grid(row=7, column=1)
+e9.grid(row=8, column=1)
+e10.grid(row=9, column=1)
+e11.grid(row=10, column=1)
+quality.grid(row=13, column=1)
+Button(master, text='Quit', command=master.destroy,width=15).grid(row=11, column=0, sticky=W, pady=4)
+Button(master, text='Find Quality', command=showQuality,width=17).grid(row=11, column=1, sticky=W, pady=4)
+Button(master, text='Project By',width=15).grid(row=14, column=0, sticky=W, pady=4)
+Button(master, text='Kumar Samyak',width=17).grid(row=14, column=1, sticky=W, pady=4)
+mainloop( )
+
+
+# In[ ]:
+
+
+
+
